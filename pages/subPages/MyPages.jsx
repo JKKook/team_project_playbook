07314ba0ab1b68b90e5e8c29510516a1338916f6
llinjae <dldlswo23@gmail.com>
2023-01-24 20:24:41 +0900

    STYLE: Navbar, DOC: 마이페이지 UI

--- pages/subPages/MyPages.jsx
+++ pages/subPages/MyPages.jsx
@@ -1,8 +1,237 @@
-import React from 'react';
-import AvatarImage from '../../src/components/atoms/AvatarImage';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { useState, useContext } from 'react';
+import Head from 'next/head';
+import avatar from '../../public/asset/user.png';
+import Image from 'next/image';
 
-const MyPages = (user, handleLogout) => {
-  return <div>{/* <AvatarImage user={user} /> */}</div>;
+const MyPageStyle = css`
+  margin-top: 2rem;
+`;
+
+const MypageSection = css`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  flex-wrap: wrap;
+  margin-left: 2rem;
+  color: #6c757d;
+  margin-bottom: 1rem !important;
+`;
+
+const DetailSection = css`
+  position: relative;
+  width: 100%;
+  padding-right: 15px;
+  padding-left: 5rem;
+`;
+
+const Text = css`
+  text-align: center !important;
+`;
+
+const Avatar = css`
+  width: 150px;
+  height: 150px;
+  overflow: hidden;
+  position: relative;
+  margin: 15px auto;
+  bottom: 1px solid #ddd;
+  border-radius: 50%;
+`;
+
+const AvatarImage = css`
+  width: 100%;
+  height: 100%;
+  display: block;
+  object-fit: cover;
+`;
+
+const ChangeImage = css`
+  position: absolute;
+  bottom: -100%;
+  left: 0;
+  width: 100%;
+  height: 40%;
+  background: #fff8;
+  text-align: center;
+  font-weight: 600;
+  color: rgb(255, 140, 45);
+  transition: 0.3 ease-in-out;
+  cursor: pointer;
+
+  &:hover {
+    bottom: -15%;
+  }
+`;
+
+const UploadFile = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  cursor: pointer;
+  color: rgb(184, 115, 115);
+`;
+
+const FormGroup = css`
+  margin-bottom: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+const FormInput = css`
+  display: block;
+  width: 100%;
+  height: calc(1.5em + 0.75rem + 2px);
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  border-radius: 0.25rem;
+  transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+`;
+
+const BtnContainer = css`
+  text-align: center;
+  margin-top: 1rem;
+`;
+
+const Button = css`
+  display: inline-block;
+  font-weight: 400;
+  text-align: center;
+  vertical-align: middle;
+  user-select: none;
+  width: 25rem;
+  background-color: transparent;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: 0.25rem;
+  color: #fff;
+  background-color: #17a2b8;
+  border-color: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #1bc4de;
+  }
+`;
+
+const MyPages = () => {
+  // 일단 모두 빈 값으로 초기값 설정
+  const initialSate = {
+    avatar: '',
+    name: '',
+    password: '',
+    cf_password: '',
+  };
+
+  const [data, setData] = useState(initialSate);
+  // const { avatar, name, password, cf_password } = data;
+
+  // const { state, dispatch } = useContext(DataContext);
+  // const { auth } = state;
+
+  // 실시간으로 input의 데이터가 입력되도록
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
+  };
+
+  // 선택한 이미지가 저장되도록
+  const changeAvatar = (e) => {
+    const file = e.target.files[0];
+    setData({ ...data, avatar: file });
+  };
+
+  return (
+    <div css={[MyPageStyle]}>
+      <Head>
+        <title>마이페이지</title>
+      </Head>
+
+      <section css={[MypageSection]}>
+        <div css={[DetailSection]}>
+          <h2 css={[Text]}>프로필</h2>
+          <div css={[Avatar]}>
+            <Image src={avatar} alt='avatar' width={150} css={[AvatarImage]} />
+            <span css={[ChangeImage]}>
+              <p>변경</p>
+              <input
+                type='file'
+                name='file'
+                css={[UploadFile]}
+                accept='image/*'
+                onChange={changeAvatar}
+              />
+            </span>
+          </div>
+
+          <div css={[FormGroup]}>
+            <label htmlFor='name'>이름</label>
+            <input
+              type='text'
+              name='name'
+              css={[FormInput]}
+              placeholder='이름을 작성하세요.'
+              onChange={handleChange}
+            />
+          </div>
+
+          <div css={[FormGroup]}>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='text'
+              name='email'
+              css={[FormInput]}
+              placeholder='Email을 작성하세요.'
+              // disabled={true}
+              onChange={handleChange}
+            />
+          </div>
+
+          <div css={[FormGroup]}>
+            <label htmlFor='password'>비밀번호</label>
+            <input
+              type='password'
+              name='password'
+              css={[FormInput]}
+              placeholder='새 비밀번호를 작성하세요.'
+              onChange={handleChange}
+            />
+          </div>
+
+          <div css={[FormGroup]}>
+            <label htmlFor='cf_password'>비밀번호 확인</label>
+            <input
+              type='password'
+              name='cf_password'
+              css={[FormInput]}
+              placeholder='새 비밀번호를 한 번 더 작성하세요.'
+              onChange={handleChange}
+            />
+          </div>
+
+          <div css={[BtnContainer]}>
+            <button css={[Button]}>업데이트</button>
+          </div>
+        </div>
+
+        <div className='detailSection'>
+          <h2 css={[Text]}>예약한 공연</h2>
+        </div>
+      </section>
+    </div>
+  );
 };
 
-export default MyPages();
+export default MyPages;
