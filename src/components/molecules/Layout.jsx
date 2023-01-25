cab5364482eee8d48d41c5dbf57267e7130d1af4
llinjae <dldlswo23@gmail.com>
2023-01-25 12:26:32 +0900

    STYLE: Loading 컴포넌트 스타일링

--- src/components/molecules/Layout.jsx
+++ src/components/molecules/Layout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Loading from '../atoms/Loading';
 
 const Layout = ({ children }) => {
   return (
     <div>
+      {/* <Loading /> */}
       <Navbar />
       {children}
     </div>
