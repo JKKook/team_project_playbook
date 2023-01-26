f742a4786b84c785a9b86d090345023d8d2129c2
llinjae <dldlswo23@gmail.com>
2023-01-26 10:29:03 +0900

    STYLE: Mypage avatar Style 변경

--- src/components/molecules/Layout.jsx
+++ src/components/molecules/Layout.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import Navbar from './Navbar';
-import Loading from '../atoms/Loading';
 
 const Layout = ({ children }) => {
   return (
     <div>
-      {/* <Loading /> */}
       <Navbar />
       {children}
     </div>
