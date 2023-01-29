694fb1e950bf5d10f7766de4641db03b72726623
JKKook <whdrnrdldia@naver.com>
2023-01-30 02:26:59 +0900

    FEAT: Add Auth form in Register when signed

--- pages/subPages/Register.jsx
+++ pages/subPages/Register.jsx
@@ -2,8 +2,7 @@
 import { css } from '@emotion/react';
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { onUserStateChange, signUp } from '../api/auth/firebase';
-import AvatarImage from '../../src/components/atoms/AvatarImage';
+import { auth, onUserStateChange, signUp } from '../api/auth/firebase';
 import {
   LoginDisplay,
   LoginExtraContainer,
@@ -24,10 +23,27 @@ import logo from '../../public/asset/playbook-logo.png';
 import Link from 'next/link';
 
 const Register = () => {
+  // 이메일, 비밀번호 , 비밀번호 확인
   const [user, setUser] = useState();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMsg, setErrorMsg] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+
+  // 오류메시지 상태 저장
+  const [emailMessage, setEmailMessage] = useState('');
+  const [passwordMessage, setPasswordMessage] = useState('');
+  const [passwordConfirmMessage, setPasswordConfirmMessage] = useState('');
+
+  // 유효성 검사
+  const [isEmail, setIsEmail] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
+  const [isPasswordConfirm, setIsPasswordConfirm] = useState(false);
+
+  const clearErrors = () => {
+    setEmail('');
+    setPassword('');
+    setPasswordConfirm('');
+  };
 
   // 쿼리 Login 컴포넌트에서 받아오기
   const router = useRouter();
@@ -41,26 +57,60 @@ const Register = () => {
 
   // 회원 가입
   const handleSignUp = async (e) => {
-    try {
-      e.preventDefault();
-      setErrorMsg('');
-      const newRegister = signUp(email, password);
-      console.log(newRegister);
-    } catch (error) {
-      // Errors : https://firebase.google.com/docs/auth/admin/errors
-      switch (error.code) {
-        case 'auth/invalid-email':
-          setErrorMsg(`${email}은 유효하지 않은 이메일 주소입니다`);
-          break;
+    e.preventDefault();
+    clearErrors();
+    const newRegister = signUp(email, password).then(router.push('/'));
+    console.log(newRegister);
+  };
 
-        case 'auth/invalid-password':
-          setErrorMsg('비밀번호는 6자리 이상이어야 합니다');
-          break;
+  // 이메일
+  const handleChangeEmail = (e) => {
+    const emailRegex =
+      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    const emailCurrent = e.target.value;
+    setEmail(emailCurrent);
 
-        case 'auth/email-already-exists':
-          setErrorMsg('이미 가입 된 계정입니다');
-          break;
-      }
+    if (!emailRegex.test(emailCurrent)) {
+      setEmailMessage(
+        '올바른 이메일 형식이 아닙니다, 다시 한 번 확인 해주세요',
+      );
+      setIsEmail(false);
+    } else {
+      setEmailMessage('사용 가능한 이메일입니다 :)');
+      setIsEmail(true);
+    }
+  };
+
+  // 비밀번호
+  const handleChangePassword = (e) => {
+    const passwordRegex =
+      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/;
+
+    const currentPassword = e.target.value;
+    setPassword(currentPassword);
+
+    if (!passwordRegex.test(currentPassword)) {
+      setPasswordMessage(
+        '숫자 + 영문자 + 특수문자 조합으로 8자리 이상 입력해주세요',
+      );
+      setIsPassword(false);
+    } else {
+      setPasswordMessage('사용가능한 비밀번호입니다');
+      setIsPassword(true);
+    }
+  };
+
+  // 비밀번호 확인
+  const handleChangePasswordConfirm = (e) => {
+    const currentPasswordConfirm = e.target.value;
+    setPasswordConfirm(currentPasswordConfirm);
+
+    if (password !== currentPasswordConfirm) {
+      setPasswordConfirmMessage('설정한 비밀번호가 일치하지 않습니다');
+      setIsPasswordConfirm(false);
+    } else {
+      setPasswordConfirmMessage('비밀번호가 동일합니다');
+      setIsPasswordConfirm(true);
     }
   };
 
@@ -90,7 +140,7 @@ const Register = () => {
           </div>
           <div css={[LoginFormContainer]}>
             {!user && (
-              <form css={[LoginFormContainer]}>
+              <form css={[LoginFormContainer]} onSubmit={handleSignUp}>
                 <p css={[RegisterFormText]}>이메일</p>
                 <input
                   css={[LoginEmailInput]}
@@ -99,8 +149,13 @@ const Register = () => {
                   placeholder='생성하고자 하는 이메일 주소를 입력하십시오'
                   required
                   value={email}
-                  onChange={handleChange}
+                  onChange={handleChangeEmail}
                 />
+                {email.length > 0 && (
+                  <span className={`message ${isEmail ? 'success' : 'error'}`}>
+                    {emailMessage}
+                  </span>
+                )}
                 <div>
                   <p css={[RegisterFormText]}>비밀번호</p>
                   <input
@@ -111,8 +166,19 @@ const Register = () => {
                     required
                     autoComplete='on'
                     value={password}
-                    onChange={handleChange}
+                    onChange={handleChangePassword}
                   />
+                  <div>
+                    {password.length > 0 && (
+                      <span
+                        className={`message ${
+                          isPassword ? 'success' : 'error'
+                        }`}
+                      >
+                        {passwordMessage}
+                      </span>
+                    )}
+                  </div>
                   <p css={RegisterFormTextSub}>
                     *비밀번호는 6자 이상이어야 하며 공백으로 시작하거나 끝날 수
                     없습니다.
@@ -121,21 +187,30 @@ const Register = () => {
                   <input
                     css={LoginEmailInput}
                     type='password'
-                    name='password'
                     required
                     autoComplete='on'
-                    value={password}
-                    onChange={handleChange}
+                    value={passwordConfirm}
+                    onChange={handleChangePasswordConfirm}
                   />
+                  <div>
+                    {passwordConfirm.length > 0 && (
+                      <span
+                        className={`message ${
+                          isPasswordConfirm ? 'success' : 'error'
+                        }`}
+                      >
+                        {passwordConfirmMessage}
+                      </span>
+                    )}
+                  </div>
                 </div>
                 <div>
                   <input
                     css={LoginSubmit}
                     type='submit'
                     value='PlayBook ID 생성'
-                    onClick={(handleSignUp) => {
-                      handleSignUp ? router.push('/') : '';
-                    }}
+                    disabled={!(isEmail && isPassword && isPasswordConfirm)}
+                    // onClick={handleRouter}
                   />
                   {user && (
                     <div>
