28a1f804a9487e0c547ef619fead412c35b0c285
llinjae <dldlswognqh@naver.com>
2023-03-11 11:04:28 +0900

    DOC

--- pages/mainPages/Performance.jsx
+++ pages/mainPages/Performance.jsx
@@ -1,16 +1,13 @@
 /** @jsxImportSource @emotion/react **/
 import { css } from '@emotion/react';
 import { useState, useEffect, useRef } from 'react';
-import { DateRange } from 'react-date-range';
-import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
-import { format, parse } from 'date-fns';
 import PerformanceList from '../../src/components/molecules/PerformanceList';
 import Loading from '../../src/components/atoms/Loading';
 import useGetPerformance from '../../src/store/server/getAPI/useGetPerformance';
-import { GoSearch, GoCalendar } from 'react-icons/go';
+import { GoSearch } from 'react-icons/go';
 
 /**---------------------- 함수 영역-------------------------------- */
 
@@ -18,13 +15,6 @@ const filterCategory = (data, category) => {
   if (!data) return;
   if (category === '') return data;
   else return data.filter((c) => c.genre === category);
-};
-
-const parseDate = (date) => {
-  const parseYear = date.getFullYear();
-  const parseMonth = date.getMonth();
-  const parseDay = date.getDate();
-  return [parseYear, parseMonth, parseDay];
 };
 
 const Performance = () => {
@@ -35,22 +25,8 @@ const Performance = () => {
     setCategory(e.target.value);
   };
 
-  // const [openDate, setOpenDate] = useState(false);
-  // const [date, setDate] = useState([
-  //     {
-  //         startDate: new Date(),
-  //         endDate: new Date(),
-  //         key: "selection",
-  //     },
-  // ]);
-
-  // Fri Feb 24 2023 11:23:48 GMT+0900 (한국 표준시)
-  const [startDate, setStartDate] = useState(new Date());
-  const [dateData, setDateData] = useState([]);
-
   const [search, setSearch] = useState('');
   const [searchData, setSearchData] = useState([]);
-  const { current: myData } = useRef(searchData);
 
   const onChange = (e) => {
     setSearch(e.target.value);
@@ -70,34 +46,16 @@ const Performance = () => {
     if (!search) setSearchData(data.data);
   }, [data, search]);
 
-  useEffect(() => {
-    if (!data) return;
-
-    setDateData(
-      data.data.filter((v) => {
-        const data = v.start.replace(/[.]/g, '');
-        const parseYear = +data.slice(0, 4);
-        const parseMonth = +data.slice(4, 6) - 1;
-        const parseDay = +data.slice(6, 8);
-
-        const compareDate = new Date(parseYear, parseMonth, parseDay);
-        if (compareDate >= startDate) return true;
-      })
-    );
-
-    console.log(dateData, parseDate(startDate));
-  }, [startDate, myData]);
-
   if (isLoading) {
     return <Loading />;
   }
 
   const renderFunction = () => {
     if (!isLoading) {
-      if (!searchData && dateData) {
-        return <PerformanceList total={dateData} />;
+      if (!searchData) {
+        return <PerformanceList />;
       }
-      if (searchData && dateData) {
+      if (searchData) {
         return <PerformanceList total={filterCategory(searchData, category)} />;
       }
       return <PerformanceList total={data.data} />;
@@ -141,50 +99,9 @@ const Performance = () => {
               </form>
             </div>
           </div>
-
-          {/* <div css={[CalendarContainer]}>
-            <span css={[CalendarUsage]}>
-              *원하시는 날짜가 있다면 캘린더를 이용해보세요 :)
-              <GoCalendar
-                onClick={() => setOpenDate(!openDate)}
-                css={[SearchInputButton]}
-              />
-            </span>
-            <span onClick={() => setOpenDate(!openDate)} css={[CalendarText]}>
-              {`${format(date[0].startDate, 'MM/dd/yyyy')} to ${format(
-                date[0].endDate,
-                'MM/dd/yyyy',
-              )}`}
-            </span>
-
-            {openDate && (
-              <DateRange
-                editableDateInputs={true}
-                onChange={(item) => setDate([item.selection])}
-                moveRangeOnFirstSelection={false}
-                ranges={date}
-                minDate={new Date()}
-                css={[Calendar]}
-              />
-            )}
-          </div> */}
-          <div css={DatePickContainer}>
-            <span css={DatePickName}>원하시는 날짜를 선택해주세요:</span>
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => {
-                setStartDate(date);
-              }}
-            />
-          </div>
         </div>
       )}
 
-      {/* {searchData && !isLoading && (
-                <PerformanceList total={filterCategory(searchData, category)} />
-            )}
-
-            {dateData && !isLoading && <PerformanceList total={dateData} />} */}
       {renderFunction()}
     </>
   );
